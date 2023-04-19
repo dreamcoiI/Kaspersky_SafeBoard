f2e087a742be1b65a942006acab32f54006b7e7a
casimira <casimira@student.21-school.ru>
2023-04-19 22:21:36 +0300

    update test.py

--- src/test.py
+++ src/test.py
@@ -5,14 +5,18 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 proccess = None
+proccess = '1'
 
-tmp = subprocess.Popen(['systemctl', 'status', '158534'])
-print(tmp.args)
+# tmp = subprocess.Popen(['systemctl', 'status', proccess])
+# print(tmp.args)
 
-# def start_process() :
-#     global proccess
-#     if proccess is not None and proccess.poll() is None:
-#         print("Process already running")
-#     else :
-#         print('Proccess not started')
-# start_process()
+def start_process() :
+    global proccess
+    proccess
+    tmp = subprocess.Popen(['systemctl', 'status', proccess])
+    if proccess is not None and tmp.returncode == 0:
+        print("Process already running")
+    else :
+        print('Proccess not started')
+        print('\n',tmp.returncode)
+start_process()

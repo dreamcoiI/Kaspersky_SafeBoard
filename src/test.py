a6d0f8c5db5f3fceafac1206760caa880a798dc5
casimira <casimira@student.21-school.ru>
2023-04-20 20:45:59 +0300

    update

--- src/test.py
+++ src/test.py
@@ -6,27 +6,44 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 proccess = None
-proccess = '300000'#пишем сюда pid для проверки
+proccess = 'telegram'#пишем сюда pid для проверки
 
 # tmp = subprocess.Popen(['systemctl', 'status', proccess])
 # print(tmp.args)
 
-def start_process() :
+def get_pid():
     global proccess
-    pid = int(proccess)
+    pid = subprocess.check_output(['pgrep',proccess])
+    print(pid)
+    return int(pid)
+# get_pid()
+
+def status_proccess() :
+    global proccess
+    pid = get_pid()
     tmp = subprocess.Popen(['systemctl', 'status', proccess])#смотрим статус данного процесса(время работы и т.д.)
     if tmp.returncode is None and psutil.pid_exists(pid):#если процесс существует и работает возвращает  true
         print("Process already running")
     else :                
         print('Proccess not started')
-start_process()
+# status_proccess()
 #все значения сверяем с выводом команды ps aux
 
-def stop_process() :
+def stop_proccess() :
     global proccess
-    pid = int(proccess)
+    pid = get_pid()
     if psutil.pid_exists(pid):
         subprocess.call(['kill',str(pid)])
     else:
         print('Proccess not started')
-stop_process()
+# stop_proccess()
+
+def start_proccess():
+    global proccess
+    pid = get_pid()
+    if psutil.pid_exists(pid) == 0 :
+        subprocess.run(["kill", "-CONT", pid])  # здесь можно заменить команду на запуск нужного процесса
+        print(f"Процесс {proccess} запущен")
+    else :
+        print(f"Процесс {proccess} не найден")
+start_proccess()

44f27445fad3aa94a2d4cb709049a5f399226440
casimira <casimira@student.21-school.ru>
2023-04-20 20:18:05 +0300

    stop process

--- src/test.py
+++ src/test.py
@@ -6,14 +6,13 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 proccess = None
-proccess = '200000'#пишем сюда pid для проверки
+proccess = '179188'#пишем сюда pid для проверки
 
 # tmp = subprocess.Popen(['systemctl', 'status', proccess])
 # print(tmp.args)
 
 def start_process() :
     global proccess
-    proccess
     pid = int(proccess)
     tmp = subprocess.Popen(['systemctl', 'status', proccess])#смотрим статус данного процесса(время работы и т.д.)
     if tmp.returncode is None and psutil.pid_exists(pid):#если процесс существует и работает возвращает  true
@@ -22,3 +21,9 @@ def start_process() :
         print('Proccess not started')
 start_process()
 #все значения сверяем с выводом команды ps aux
+
+def stop_process() :
+    global proccess
+    pid = int(proccess)
+    subprocess.call(['kill',str(pid)])
+stop_process()

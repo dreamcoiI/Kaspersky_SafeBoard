40fd0d1b5f211232f8f5271fde2f66ca2d9e1908
Denis <116588516+dreamcoiI@users.noreply.github.com>
2023-04-23 23:15:20 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,13 +10,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
 app = FastAPI()
 
 process = None
-
-
-import os
 
 def start_process():
     global process
@@ -24,7 +20,6 @@ def start_process():
         raise HTTPException(status_code=400, detail="Process already running")
     path = os.path.abspath(os.path.dirname(__file__))  # получаем абсолютный путь к директории, в которой находится скрипт
     process = subprocess.Popen(["python", os.path.join(path, "word_count.py"), os.path.join(path, "text.txt"), os.path.join(path, "result.txt")], cwd=path)
-
 
 def stop_process():
     global process
@@ -39,7 +34,6 @@ def stop_process():
         process.kill()
     process = None
 
-
 def get_process_status():
     global process
     if process is None:
@@ -48,7 +42,6 @@ def get_process_status():
         process = None
         return {"status": "stopped"}
     return {"status": "running"}
-
 
 def get_process_result():
     global process
@@ -61,16 +54,13 @@ def get_process_result():
         result = f.read()
     return {"result": result}
 
-
-
 @app.post("/api/{process_name}/start")
 def start(process_name: str):
     process_name=str(process_name)
     if process_name != "word_count":
         raise HTTPException(status_code=404, detail="Process not found")
     start_process()
-    return {"status": "start"}
-
+    return {"status": "ok"}
 
 @app.post("/api/{process_name}/stop")
 def stop(process_name: str):
@@ -80,20 +70,17 @@ def stop(process_name: str):
     stop_process()
     return {"status": "ok"}
 
-
 @app.get("/api/{process_name}")
 def status(process_name: str):
     if process_name != "word_count":
         raise HTTPException(status_code=404, detail="Process not found")
     return get_process_status()
 
-
 @app.get("/api/{process_name}/result")
 def result(process_name: str):
     if process_name != "word_count":
         raise HTTPException(status_code=404, detail="Process not found")
     return get_process_result()
-
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item():
@@ -118,8 +105,6 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/api/docs", include_in_schema=False)
 async def get_documentation():
     return templates.TemplateResponse("documentation.html", {"request": "localhost:8000"})
-
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
